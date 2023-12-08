a55f0ebea1e93fa10ddf1b001f65ceb76a5706f3
b2b <rodinamobark3@gmail.com>
2023-12-08 16:26:52 +0200

    creation

--- app/src/main/java/com/example/realtimedatabasefirebase/NoteFragment.kt
+++ app/src/main/java/com/example/realtimedatabasefirebase/NoteFragment.kt
@@ -11,6 +11,7 @@ class NoteFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
 
+
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_note, container, false)
